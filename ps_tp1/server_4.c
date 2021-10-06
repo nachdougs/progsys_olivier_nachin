6e8352c57ff294aee63880dee53a55d93c2bf83f
Olivier Nachin <olivier.nach@gmail.com>
2021-10-06 21:41:51 +0200

    server_4.c 2e version

--- ps_tp1/server_4.c
+++ ps_tp1/server_4.c
@@ -9,12 +9,23 @@ Par contre, lorsqu’on tue le père, le fils n’est pas tué !
 Il faut donc faire en sorte que lorsqu’on tue le père, le fils soit tué aussi.
 */
 
+
+void stop_handler(int sig)
+{
+    printf("Signal reçu : %d\nOn tue le fils\n", sig);
+    kill(getpid() + 1, SIGTERM); // je n’ai pas trouvé comment faire pour tuer le fils plus proprement...
+    printf("On tue le père à présent\n");
+    exit (EXIT_SUCCESS);
+}
+
+
 int main(void)
 {
     int     id;
     int     rand_int_p;
     int     rand_int_c;
     int     tube[2];
+    struct  sigaction action;
 
     printf("Démarrage du programme\n");
     pipe(tube);
@@ -28,8 +39,12 @@ int main(void)
     {
         // Code père
         close(tube[0]);
+
+        action.sa_handler = stop_handler;
+        sigaction(SIGTERM, &action, NULL);
+        
         while (1)
-        { 
+        {
         rand_int_p = rand() % 100;
         write(tube[1], &rand_int_p, sizeof(int));
         printf("Je suis le père et je viens d’envoyer un nombre aléatoire à mon fils.\n");
@@ -39,13 +54,13 @@ int main(void)
     else
     {
         // Code fils
+        
         close(tube[1]);
         while (1)
         {
-        if (read(tube[0], &rand_int_c, sizeof(int)) != -1)
-            printf("Je suis le fils et le père m’a envoyé la valeur : %d\n", rand_int_c);
-        else
-            break;
+        sleep(1);
+        read(tube[0], &rand_int_c, sizeof(int));
+        printf("Je suis le fils et le père m’a envoyé la valeur : %d\n", rand_int_c);
         }
     }
     return (EXIT_SUCCESS);
